e7b9b56452bf8b9ec8665d8bcfe877a860fdef28
Bastian Alejandro Silva Fuenzalida <bastian.silva@globant.com>
2022-03-17 17:46:27 -0300

    [+] added comments

--- environment.py
+++ environment.py
@@ -2,16 +2,20 @@ from selenium import webdriver
 import os
 import json
 
+# set up config file and task id from the environment variables
 config_file = os.environ['CONFIG_FILE'] if 'CONFIG_FILE' in os.environ else 'config/single.json'
 task_id = int(os.environ['TASK_ID']) if 'TASK_ID' in os.environ else 0
 
+# open the config file
 with open(config_file) as data_file:
     config = json.load(data_file)
 
+# set up user and key from config file
 username = config['user']
 access_key = config['key']
 
 
+# set up before feature
 def before_feature(context, feature):
     desired_capabilities = config['environments'][task_id]
 
@@ -24,6 +28,7 @@ def before_feature(context, feature):
         command_executor=f"https://{username}:{access_key}@hub.browserstack.com/wd/hub")
 
 
+# set up after feature
 def after_feature(context, feature):
     if context.failed is True:
         context.browser.execute_script(
